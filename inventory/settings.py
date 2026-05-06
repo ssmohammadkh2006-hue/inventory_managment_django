6f7e0b7c2c9fd1f17cb0c35f8f1893decb069a20
Mohammad_ALnimrawi <ssmohammad.kh2006@gmail.com>
2026-05-06 18:56:11 +0300

    dploy

--- inventory/settings.py
+++ inventory/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7w*(j^$a)tb-sldkbi09fr@&jotv)nw(-yt8ka83gg4epch(yp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
